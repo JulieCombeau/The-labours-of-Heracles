9d3569e0a62bdd15604a73aede58de22007aefa4
Julie Combeau <combeau.julie@gmail.com>
2022-03-31 11:55:48 +0200

    First push

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+import { Fighter } from "./src/Fighter.js";
+
+const heracles = new Fighter("Heracles", 20, 6);
+const lion = new Fighter("Nemean Lion", 11, 13);
+
+let round = 1;
+
+while (heracles.life > 0 && lion.life > 0) {
+  console.log(`Round n° ${round}`);
+  heracles.fight(lion);
+  if (lion.life > 0) lion.fight(heracles);
+  round++;
+}
+
+heracles.life == 0 ? console.log("Lion Win") : console.log("Heracles Win");
+lion.life > 0 ? console.log("Heracles is dead") : console.log("Lion is dead");
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,22 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  fight(joueur) {
+    let attack = Math.round(Math.random() * this.strength);
+    let defense = attack - joueur.dexterity < 0 ? 0 : attack - joueur.dexterity;
+    joueur.life = joueur.life - defense < 0 ? 0 : joueur.life - defense;
+    console.log(
+      `${this.name} attack ${joueur.name} ${joueur.name} : ${joueur.life}`
+    );
+  }
+}
+
+export { Fighter };
